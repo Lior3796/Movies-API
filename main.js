e27c0143570e0b19c8391af776ecd4938a52f98c
Lior3796 <liorlsa9@gmail.com>
2021-03-21 00:31:45 +0200

    add "no movie" alert

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let baseApi = 'https://api.tvmaze.com/search/shows?q=';
-function getShow(show) {
 
+function getShow(show) {
   return fetch(`${baseApi}${show}`, {})
     .then((response) => {
       return response.json();
@@ -9,41 +9,59 @@ function getShow(show) {
 }
 
 async function showSearch() {
+  let favoriteShow;
   try {
+
     let containter = document.getElementById("container");
     containter.innerText = '';
     let movieChoice = document.getElementById("floatingInputValue").value;
-    console.log(movieChoice);
-    let favoriteShow = await getShow(movieChoice);
+
+    favoriteShow = await getShow(movieChoice);
     favoriteShow.forEach(element => {
       let showObject = element.show;
-      
+
       for (let key in showObject) {
         switch (key) {
           case "url":
             containter.innerHTML += `<div><a href="${showObject[key]}">link</></div>`;
             break;
           case "genres":
+            
+            if(showObject[key].length === 0) {
+            containter.innerHTML += "no genre";
+             }
+            else{
+              
             containter.innerHTML += `<div><h1>${showObject[key][0]}</h1></div>`;
+
+            }
             break;
+           
           case "name":
+
             containter.innerHTML += `<div><h1>${showObject[key]}</h1></div>`;
+
             break;
           case "image":
+            if(showObject[key].medium === null){
+              console.log("no image movie");  
+            }
             containter.innerHTML += `<img src="${showObject[key].medium}"></img>`
-          default:
-            break;
+
         }
 
       }
       console.log(element.show);
-     
+
     });
   }
   catch (res) {
     console.log(res);
   }
   finally {
+    if(favoriteShow.length === 0){
+      document.write("no movie in the list")
+    }
     console.log("success");
   }
 }
